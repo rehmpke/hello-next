b498fedfbe67846b07e30f731a42b8f0b0cf1ca8
rehmpke <rehmpke@gmail.com>
2018-11-27 16:30:22 -0600

    Import next/router into post.js

--- pages/post.js
+++ pages/post.js
@@ -1,11 +1,17 @@
 import {withRouter} from 'next/router'
 import Layout from '../components/MyLayout.js'
 
-const Page = withRouter((props) => (
-  <Layout>
+const Content = withRouter((props) => (
+  <div>
     <h1>{props.router.query.title}</h1>
     <p>This is the blog post content.</p>
-  </Layout>
+  </div>
 ))
+
+const Page = (props) => (
+    <Layout>
+       <Content />
+    </Layout>
+)
 
 export default Page
