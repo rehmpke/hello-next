0b3c2f206e1e12b627cc8494e5d2828ee0006e07
rehmpke <rehmpke@gmail.com>
2018-11-28 12:17:49 -0600

    Change server route

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.prepare()
 .then(() => {
   const server = express()
 
-  server.get('/p/:id', (req, res) => {
+server.get('/p/:id', (req, res) => {
     const actualPage = '/post'
-    const queryParams = { title: req.params.id } 
+    const queryParams = { id: req.params.id }
     app.render(req, res, actualPage, queryParams)
-  })
+})
 
   server.get('*', (req, res) => {
     return handle(req, res)
